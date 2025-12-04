a42cb1d24a2572ce66d27a416633976579519b36
GNHee_Cho <rmsgml0915@naver.com>
2025-12-04 22:50:42 +0900

    q

--- success.js
+++ success.js
@@ -35,11 +35,53 @@ var products = [
     { id : 2, price : 60000, title : 'Black Monastery' }
   ];
 
-products.forEach((product)=>{
-    console.log(product.price);
-})
 for (let i = 0; i < products.length; i++) {
     document.querySelectorAll('.card-body h5')[i].innerHTML=products[i].title; 
     document.querySelectorAll('.card-body p')[i].innerHTML=`가격 : ${products[i].price}`
     
 }
+var pants_size =[28,30,32,34,36,38,40];
+const form_select = document.querySelectorAll('.form-select');
+let shirts_html = '<option>95</option><option>100</option>';
+form_select[0].addEventListener('change',(e)=>{
+    if(e.currentTarget.value=='셔츠'){
+        form_select[1].classList.remove('hide');
+        form_select[1].innerHTML = shirts_html;
+
+    }else if(e.currentTarget.value=='바지'){
+        form_select[1].classList.remove('hide');
+        pants_size.forEach((size)=>{        
+            const html_size =`<option>${size}</option>`; 
+            form_select[1].insertAdjacentHTML('beforeend',html_size);
+
+        })
+    }else{
+       form_select[1].classList.add('hide');
+    }
+    
+})
+
+function average(a,b){
+    let avg=0;
+    for(let i=0; i<=a.length-1; i++){        
+        avg=avg+a[i];
+    }
+    avg=avg/a.length;
+    if(avg<=b){
+        console.log(`평균보다 ${b-avg}점이 올랐네요`)
+    }else{
+        console.log(`평균보다 ${avg-b}점이 떨어졌네요 재수추천`)
+    }
+
+}
+fetch('https://codingapple1.github.io/price.json')
+  .then(res => res.json())
+  .then(function(data){
+    console.log(data.price)
+  })
+  .catch(function(error){
+    console.log('실패함')
+  });
+
+
+
